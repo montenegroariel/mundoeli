09fad0205abb5e87eb366aab9887f10cab0bfb60
Ariel Montenegro <ariel.montenegro@mirgor.com>
2025-05-03 19:59:14 -0300

    app

--- config/settings/production.py
+++ config/settings/production.py
@@ -78,7 +78,7 @@ STORAGES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Mundo Eli <noreply@pos.mundoeli.com.ar>",
+    default="Mundo Eli <noreply@mundoeli.com.ar>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
