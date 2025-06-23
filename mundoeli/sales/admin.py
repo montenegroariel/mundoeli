6c43bd9187a9f515e4a3629d03a0e34db906da0b
Ariel Montenegro <ariel.montenegro@mirgor.com>
2025-06-23 08:51:35 -0300

    sala detail

--- mundoeli/sales/admin.py
+++ mundoeli/sales/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Sale
 
-# Register your models here.
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ("date", "total")
+    list_filter = ("date", "total")
+    search_fields = ("date", "total")
+    readonly_fields = ("date", "total")
