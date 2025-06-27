d98af9941db12aca597aa9c3f2d1036a7a853c0b
Ariel Montenegro <ariel.montenegro@mirgor.com>
2025-06-27 19:44:56 -0300

    description null true

--- mundoeli/products/models.py
+++ mundoeli/products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     barcode = models.CharField(max_length=50, unique=True, verbose_name="Código de Barras")
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
